ad6c4f28771f26a9c33484b07ab5898b53c16a3f
adawolfs <adawolfs@gmail.com>
2021-09-17 22:48:04 -0600

    implement camera height change

---
+++ mars.js
@@ -0,0 +1,25 @@
+
+AFRAME.registerComponent('mars-camera', {
+    init: function() {
+        var data = this.data;
+        var el = this.el;
+        this.el.addEventListener('raycaster-intersection', els => {
+            this.intersection = els.detail.intersections[0];
+        });
+        this.el.addEventListener('raycaster-intersection-cleared', els => {
+            console.log('Player hit cleard!');
+            this.intersection = null;
+        });
+    },
+    update: function() {
+    },
+    tick: function (time, timeDelta) {
+        if (this.intersection ) {
+            if (this.intersection.point.y > 0){
+                this.el.object3D.position.y += 0.05;
+            } 
+        } else if (this.el.object3D.position.y > 1 ) {
+            this.el.object3D.position.y -= 0.05;
+        }
+    }
+})
